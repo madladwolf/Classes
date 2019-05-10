72ccfa3d15a7ec455a642cef3ea32735db3aaff4
madladwolf <48660166+madladwolf@users.noreply.github.com>
2019-05-10 10:29:30 +0100

    Ex. Nr.1 C#
    Object and Class Training.

---
+++ example1/example1/Ponto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example1
+{   // Class Ponto (X & Y as atributes) 
+    class Ponto
+    {
+        //auto properties (let the program know if you
+        //give a value is set
+        //ask for value is get
+        public int X { get; set; }
+        public int Y { get; set; }
+        //Builders
+        public Ponto() { this.X = 0;this.Y = 0; }
+        public Ponto(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        //Overriding ToString() so it does what we want
+        public override string ToString()
+        {
+            return "Ponto (" + X + "," + Y + ")";
+        }
+    }
+}
---
+++ example1/example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new object p1 with the default values of 0,0
+            Ponto p1 = new Ponto();
+            //Create a new object Ponto2 with the same memory position of p1
+            Ponto p2 = p1;
+            //define X of p2 as "2"
+            p2.X = 2;
+            //calling ToString on p1
+            Console.WriteLine(p1.ToString());
+            //stopping the program to end abruptly
+            Console.ReadKey();
+        }
+    }
+}
