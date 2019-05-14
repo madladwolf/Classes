a6a34f2aef31301e720ddf84b20e78936c8ffb12
madladwolf <48660166+madladwolf@users.noreply.github.com>
2019-05-14 10:25:32 +0100

    5119-FT02
    https://sites.google.com/site/atecgrsi/

---
+++ 5119-FT02/5119-FT02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5119_FT02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Data
+    {
+        int mes, dia, ano;
+        public Data()
+        {
+            dia = 0;
+            mes = 0;
+            ano = 0;
+        }
+        public Data(int diaA, int mesB, int anoC)
+        {
+            if (mesB == 2)
+            {
+                if(diaA <)
+            }
+        }
+        public Data(Data previousData)
+        {
+            dia = previousData.dia;
+            mes = previousData.mes;
+            ano = previousData.ano;
+        }
+    }
+}
