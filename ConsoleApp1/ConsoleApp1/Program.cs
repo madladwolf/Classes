523be4398933e1b7af0d1efa58ec2b9608238de8
madladwolf <48660166+madladwolf@users.noreply.github.com>
2019-05-14 10:30:17 +0100

    Console app 1
    https://sites.google.com/site/atecgrsi/

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read&Write to files (Se vencimento superior que 1000)
+            StreamReader read = new StreamReader(@"C:\Users\t0114312\Documents\files\vencimentos.txt");
+            StreamWriter wr = new StreamWriter(@"C:\Users\t0114312\Documents\files\supmil.txt");
+            int aux1 = 0;
+            while (!read.EndOfStream)
+            {
+                string linha = read.ReadLine();
+                string[] campos = linha.Split(' ');
+                int aux0 = campos[2].Length;
+                if (aux0 >= 4)
+                {
+                    wr.Write(campos[0] + ' ' + campos[1] + ' ' + campos[2]);
+                    aux1++;
+                }
+            }
+            if(aux1 == 0)
+            {
+                wr.Write("Nao existem valores a apresentar");
+            }
+            read.Close();
+            wr.Close();
+        }
+    }
+}
